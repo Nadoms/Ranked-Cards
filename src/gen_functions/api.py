0465028b0061591420a7a44632714bf104aa62ec
Nader Takruri <nader.takruri@cambridgeconsultants.com>
2024-12-30 11:16:35 +0000

    Commit progress

--- src/gen_functions/api.py
+++ src/gen_functions/api.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import requests
 
 
@@ -38,17 +39,153 @@ class APINotFoundError(APIError):
         return f"APINotFoundError: {self.url}"
 
 
-def get(directory_url: str, params: dict = None):
-    url = API_URL + directory_url
-    response = requests.get(url, headers=HEADERS, params=params)
+class APITimeoutError(APIError):
 
-    if response["status"] == "error":
-        if response["data"] == "Too many requests":
-            raise APIRateLimitError(url)
-        elif response["data"] in NOT_FOUND_DATA:
-            raise APINotFoundError(url)
-        else:
-            raise APIError(url)
+    def __init__(self, url: str):
+        super().__init__(url)
 
-    response.raise_for_status()
-    return response.json()
+    def __str__(self):
+        return f"APITimeoutError: {self.url}"
+
+
+class API():
+
+    def __init__(
+        self,
+        directory: str,
+    ):
+        self.url : str = f"{API_URL}{directory}?"
+
+    def add(self, parameters: dict):
+        formatted_parameters = []
+        for key in parameters:
+            if key is None:
+                continue
+            if parameters[key] == "":
+                formatted_parameters.append(parameters["key"])
+            else:
+                formatted_parameters.append(f"{key}={parameters['key']}")
+        self.url += "&".join(formatted_parameters)
+
+
+    def get(directory_url: str) -> dict:
+        url = API_URL + directory_url
+
+        try:
+            response = requests.get(url, headers=HEADERS, timeout=5)
+        except requests.exceptions.Timeout:
+            raise APITimeoutError(url)
+
+        if response["status"] == "error":
+            if response["data"] == "Too many requests":
+                raise APIRateLimitError(url)
+            elif response["data"] in NOT_FOUND_DATA:
+                raise APINotFoundError(url)
+            else:
+                raise APIError(url)
+
+        return response.json()["data"]
+
+
+class User(API):
+
+    def __init__(
+        self,
+        name: str,
+        *,
+        season: Optional[int] = None,
+    ):
+        self.name = name
+        self.season = season
+        self.set_url()
+
+    def set_url(self):
+        self.url = f"users/{self.name}?"
+        if self.season:
+            self.url += f"season={self.season}"
+
+
+class Matches(API):
+
+    def __init__(
+        self,
+        page: int,
+        count: int,
+        type: int,
+        season: int,
+    ):
+        self.page = page
+        self.count = count
+        self.type = type
+        self.season = season
+
+
+class RecentMatches(Matches):
+
+    def __init__(
+        self,
+        *,
+        page: Optional[int] = None,
+        count: int = 50,
+        type: Optional[int] = None,
+        season: Optional[int] = None,
+        excludedecay: bool = None,
+    ):
+        super().__init__(page=page, count=count, type=type, season=season)
+        self.excludedecay = excludedecay
+
+
+class UserMatches(Matches):
+
+    def __init__(
+        self,
+        name: str,
+        *,
+        page: Optional[int] = None,
+        count: int = 50,
+        type: Optional[int] = None,
+        season: Optional[int] = None,
+        exclude_decay: bool = None,
+    ):
+        super().__init__(page=page, count=count, type=type, season=season)
+        self.name = name
+        self.exclude_decay = exclude_decay
+
+
+class VersusMatches(Matches):
+
+    def __init__(
+        self,
+        name_1: str,
+        name_2: str,
+        *,
+        page: Optional[int] = None,
+        count: int = 50,
+        type: Optional[int] = None,
+        season: Optional[int] = None,
+    ):
+        super().__init__(page=page, count=count, type=type, season=season)
+        self.name_1 = name_1
+        self.name_2 = name_2
+
+
+class Match(API):
+
+    def __init__(
+        self,
+        match_id: int,
+    ):
+        self.match_id = match_id
+
+
+class Versus(API):
+
+    def __init__(
+        self,
+        name_1: str,
+        name_2: str,
+        *,
+        season: Optional[int] = None,
+    ):
+        self.name_1 = name_1
+        self.name_2 = name_2
