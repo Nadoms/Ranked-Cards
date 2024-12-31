6db9fa01214818ef5bf0276f73063ec20883a86a
Nader Takruri <nader.takruri@cambridgeconsultants.com>
2024-12-31 14:18:00 +0000

    Remove headers

--- src/gen_functions/games.py
+++ src/gen_functions/games.py
@@ -94,9 +94,6 @@ async def get_detailed_matches(player_response, season, min_comps, target_games)
     name = player_response["nickname"]
     uuid = player_response["uuid"]
     response = "PLACEHOLDER"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:110.0) Gecko/20100101 Firefox/110.0."
-    }
 
     while True:
         response = api.UserMatches(
