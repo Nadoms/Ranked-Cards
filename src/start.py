2bf74d189053ae4f5812cc7b39661d622aeeab16
Nadoms <n.takrouri@hotmail.com>
2025-12-11 19:58:30 +0000

    Reinstall rankedutils on startup

--- src/start.py
+++ src/start.py
@@ -10,6 +10,12 @@ LOG = ROOT / "logs" / f"bot_{datetime.now().strftime('%m%d-%H%M')}.log"
 
 load_dotenv(ROOT.parent / ".env")
 
+subprocess.run(
+    f"pip3 uninstall rankedutils -y && pip3 install git+https://github.com/Nadoms/ranked-utils.git",
+    text=True,
+    shell=True,
+)
+
 process = subprocess.run(
     f"python3 -u {BOT} 2>&1 | tee {LOG}",
     text=True,
