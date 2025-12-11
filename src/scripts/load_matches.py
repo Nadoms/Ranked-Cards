184b579e6c04ed83db187576f5ed42e89c66a7fb
Nadoms <n.takrouri@hotmail.com>
2025-12-11 21:51:30 +0000

    Function around cheated matches during loads

--- src/scripts/load_matches.py
+++ src/scripts/load_matches.py
@@ -16,6 +16,12 @@ async def spam_redlime(start, limit):
     old_additions = api.Match._additions
     new_additions = 0
 
+    async def safe_get_async(id):
+        try:
+            return await api.Match(id=id).get_async()
+        except (api.APINotFoundError):
+            return None
+
     while True:
         print(f"Loading matches {i} to {i + step_size} / Total: {new_additions}")
         k = 0
@@ -23,18 +29,20 @@ async def spam_redlime(start, limit):
         try:
             matches = await asyncio.gather(
                 *[
-                    api.Match(id=j).get_async()
+                    safe_get_async(j)
                     for j in range(i, i + step_size)
                 ]
             )
+            matches = [m for m in matches if m is not None]
             k += step_size
             if new_additions >= limit:
                 new_additions = api.Match.commit() - old_additions
                 print("Limit reached, total loaded:", new_additions)
                 return i
-        except (api.APIRateLimitError, api.APIError):
+        except (api.APIError) as e:
             new_additions = api.Match.commit() - old_additions
-            print("Rate limit reached, total loaded:", new_additions)
+            print(e)
+            print("Error occurred, total loaded:", new_additions)
             return i
 
         new_additions = api.Match.commit() - old_additions
