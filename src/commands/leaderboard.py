f1c3d2f057849a8f8ed5c08b31b05468afc9e580
Nadoms <n.takrouri@hotmail.com>
2026-09-21 23:08:04 +0000

    Yet another minor aesthetic change

--- src/commands/leaderboard.py
+++ src/commands/leaderboard.py
@@ -268,6 +268,7 @@ class LBEmbeds():
         self.header = header
         temp = header.replace(" | ", "/")[1:]
         temp = re.sub(r"(\w|\s)", ".", temp)
+        temp = header.replace("/", " | ")[1:]
         self.separator = " " + temp
         self.embeds = []
 
